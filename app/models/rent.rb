90a584de45d77ff8e595563f32474dc01b3bf7fd
yaswanth <yaswanthbonda@gmail.com>
2016-02-05 00:55:27 -0500

    corrected a validation

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,5 +3,5 @@ class Rent < ActiveRecord::Base
   
   #validates :rent_time_period_in_months, length: { in: 11..121 }
   validates :renter_name, :rent_time_period_in_months, :rent_cost_in_dollars, :deposit_amount, presence: true
-  validates :rent_time_period_in_months, inclusion: { in: %w(11..121), message: ": %{value} is not a valid input, its range is 12 to 120" }
+  #validates :rent_time_period_in_months, inclusion: { in: %w(11..121), message: ": %{value} is not a valid input, its range is 12 to 120" }
 end
